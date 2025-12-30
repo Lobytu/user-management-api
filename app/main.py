bede25457c97f7ce06cb02d57ea9d1c4c1e83a7d
Lobytu <stk18tao@hotmail.com>
2025-12-30 21:16:08 +0000

    Add create user endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from . import models, schemas, crud
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/users", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db, user)
+
